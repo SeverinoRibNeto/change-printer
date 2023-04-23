61b23eee49e92fc785cb0d6de75ada1530fe4c44
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2023-04-23 13:52:50 -0300

    Add functions to change printers

---
+++ printers.py
@@ -0,0 +1,13 @@
+import win32print
+from typing import List
+
+def lista_impressoras_instaladas()-> List[str]:
+    #procura impressoras do sistema e retorna a lista
+    return [impressora[2] for impressora in win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL, None, 1)]
+
+def definir_impressora_padrao(nome_impressora_padrao:str)-> bool:
+    #testa se impressora existe
+    if(not(nome_impressora_padrao in lista_impressoras_instaladas())):#Se o nome da impressora não tiver nas impressoras listadas, lança erro
+        raise ValueError ("Impressora não instalada no sistema")
+    # define a nova impressora padrão
+    win32print.SetDefaultPrinter(nome_impressora_padrao)
