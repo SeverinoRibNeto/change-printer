58b227ea10fbb1c42d39d944453d98a941438053
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2023-04-28 13:24:13 -0300

    Add func janela_esta_ativa e pegar_nome_processo

--- model_printers.py
+++ model_printers.py
@@ -1,4 +1,10 @@
 import win32print
+import win32gui
+import win32process
+import win32con
+import win32api
+import psutil
+import os
 from typing import List
 
 
@@ -23,3 +29,33 @@ class Printer:
         # define a nova impressora padrão
         win32print.SetDefaultPrinter(nome_impressora_padrao)
         return True
+    
+    def janela_esta_ativa(self, nome_processo:str)->bool:
+        #verifica a janela ativa
+        janela_ativa = win32gui.GetForegroundWindow()
+        #obter id do processo a partir do identificador da janela
+        pid = win32process.GetWindowThreadProcessId(janela_ativa)[1]
+
+        #obtem o handler do processo a partir do identificador
+        handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, 
+                                          False, 
+                                          pid)
+
+        #obter nome do executavel do processo a partir do handle
+        exe_file = win32process.GetModuleFileNameEx(handle, 0)
+
+        print(exe_file)
+        process_name = psutil.Process(pid).name()
+        print("Nome do processo da janela ativa:", process_name)
+        if process_name.upper() == nome_processo.upper():
+            return True
+        
+        return False
+    
+    def pegar_nome_processo(self, caminho_processo:str) -> str:
+        return os.path.basename(caminho_processo).upper() #Retorna o nome do processo pelo caminho
+
+
+if __name__ == "__main__":
+    impressora = Printer()
+    print(impressora.janela_esta_ativa('Code.exe'))
