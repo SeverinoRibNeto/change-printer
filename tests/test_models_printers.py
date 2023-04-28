be72e5fa7b1150b844806cb6d09dfa05cd81e838
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2023-04-28 13:17:40 -0300

    Add teststings

--- tests/test_models_printers.py
+++ tests/test_models_printers.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from printers import *
+from model_printers import *
 import win32print
 import pytest
 
@@ -10,8 +10,43 @@ def test_mudando_a_impressora_padrao_para_fax():
     current_printer = win32print.GetDefaultPrinter()
     new_default_printer = 'Fax'
     #When
-    definir_impressora_padrao(new_default_printer)
+    model_printer = Printer()
+    model_printer.definir_impressora_padrao(new_default_printer)
 
     #Then
     assert win32print.GetDefaultPrinter() == new_default_printer
-    definir_impressora_padrao(current_printer)
+    model_printer.definir_impressora_padrao(current_printer)
+
+def test_verifica_janela_ativa_e_igual_a_Code():
+    #Given
+    processo = "Code.exe" #Nome da janela ativa e processo esperado
+    
+    #When
+    model_printer = Printer()
+    
+
+    #Then
+    assert model_printer.janela_esta_ativa(processo) == True
+    assert model_printer.janela_esta_ativa("winword.exe") == False
+
+def test_pega_nome_do_processo_do_caminho_e_mostra_se_e_igual_a_Code():
+    #Given
+    processo = "Code.exe" #Nome da janela ativa e processo esperado
+    caminho= "C:\\Users\\SeverinoRibeiroNeto\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+    
+    #When
+    model_printer = Printer()
+    
+    #then
+    assert model_printer.pegar_nome_processo(caminho) == processo.upper()
+
+def test_pega_nome_do_processo_do_caminho_e_mostra_se_igual_a_Uptade_exe():
+    #Given
+    processo ="Update.exe"
+    caminho="C:\\Users\\SeverinoRibeiroNeto\\AppData\\Local\\Discord\\Update.exe"
+
+    #When
+    model_printer = Printer()
+
+    #then
+    assert model_printer.pegar_nome_processo(caminho) == processo.upper()
