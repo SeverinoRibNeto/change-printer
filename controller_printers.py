2c50536346f3913600be218fc5d06faa3e828990
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2023-04-29 16:50:06 -0300

    Add stop function and pub.subscribe

--- controller_printers.py
+++ controller_printers.py
@@ -5,6 +5,7 @@ from view_printers import GUI
 from typing import List
 class PrinterController:
     def __init__(self) -> None:
+        self.running = False
         printer = Printer()
         self.app = wx.App()
         self.frame = GUI(None)
@@ -14,11 +15,12 @@ class PrinterController:
         pub.subscribe(self.saveConfig, "Save_Config_Pressed")
         pub.subscribe(self.loadConfig, "Load_Config_Pressed")
         pub.subscribe(self.start, "Start_Pressed")
+        pub.subscribe(self.stop, "Stop_Pressed")
 
 
 
     def saveConfig(self, caminho_processo, impressora_selecionada) -> bool:
-        #Salva as configurações em um arquivo txt de nome config.txt
+        #Salva as configurações em um arquivo txt de nome config.txt    
         try:
             with open('config.txt', "w") as f:
                 f.write(f'caminho={caminho_processo}')
@@ -29,13 +31,22 @@ class PrinterController:
         print("Save_Config_Pressed - Controller")
 
     def loadConfig(self) -> None:
-        print(self.retorna_caminho_impressora_config())
+        config = self.retorna_caminho_impressora_config()
+        self.frame.m_choice2.SetStringSelection(config[1])
+        self.frame.m_filePicker1.SetPath(config[0])
         print("Load_Config_Pressed - Controller")
 
     def start(self, caminho_processo, impressora_selecionada) -> bool:
         print("Start_Pressed - Controller")
+        self.frame.m_staticText31.SetLabelText(text="Programa em funcionamento")
+        self.running = True
         print(impressora_selecionada, caminho_processo)
         return True
+
+    def stop(self):
+        self.running = False
+        self.frame.m_staticText31.SetLabelText(text="Programa Parado")
+        print("Stop_Pressed - Controller")
 
     def retorna_caminho_impressora_config(self) -> List[str]:
         try:
