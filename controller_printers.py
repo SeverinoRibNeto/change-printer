b6bcbae0dded336fedb378247c7c52f83e8c8da3
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2023-05-02 15:43:25 -0300

    Added thread to monitor

--- controller_printers.py
+++ controller_printers.py
@@ -2,21 +2,50 @@ from pubsub import pub
 import os
 import wx
 import wx.xrc
+import asyncio
+import time
+import win32print
+from threading import Event
+import threading
 from model_printers import Printer
 from view_printers import GUI
 from typing import List
 
 
+class PrintThread(threading.Thread):
+    def __init__(self, caminho_processo:str, impressora:str) -> None:
+        threading.Thread.__init__(self)
+        self.caminho = caminho_processo
+        self.impressora = impressora
+        self.default_printer = win32print.GetDefaultPrinter()
+        self.to_stop = False
+    
+    def run(self) -> None:
+        while True:
+            time.sleep(0.5)
+            self.check_active_window_update_printer()
+            if self.to_stop:
+                return None
+
+    def check_active_window_update_printer(self)->None:
+        printer = Printer()
+        process_name = printer.pegar_nome_processo(self.caminho)
+        if printer.janela_esta_ativa(process_name):
+            printer.definir_impressora_padrao(self.impressora)
+        elif not printer.janela_esta_ativa(process_name):
+            printer.definir_impressora_padrao(self.default_printer)
+        print("Checking active window")
+
 class PrinterController:
     def __init__(self) -> None:
         self.running = False  # Abre o programa com a condição falsa
-
-        printer = Printer()  # declaração da model printer
+        self.task = None
+        self.printer = Printer()  # declaração da model printer
         self.app = wx.App()  # criação da interface gráfica
         self.frame = GUI(None)  # chamada da interface gráfica
 
         # preenche a lista de impressoras do sistema
-        self.frame.m_choice2.SetItems(printer.lista_impressoras_instaladas())
+        self.frame.m_choice2.SetItems(self.printer.lista_impressoras_instaladas())
         if (not os.path.exists('config.txt')):
             # Cria arquivo vazio com a configuração da impressora
             self.criar_arquivo_config('', '')
@@ -26,6 +55,8 @@ class PrinterController:
         pub.subscribe(self.loadConfig, "Load_Config_Pressed")
         pub.subscribe(self.start, "Start_Pressed")
         pub.subscribe(self.stop, "Stop_Pressed")
+
+
 
     def saveConfig(self, caminho, impressora) -> bool:
         # salva caminho e impressora no arquivo config.txt
@@ -46,18 +77,17 @@ class PrinterController:
         self.frame.m_choice2.Disable()
         self.frame.m_filePicker1.Disable()
 
-    def start(self, caminho: str, impressora: str) -> bool:
-        # TODO: Implementar o looping de checagem de programa na model
-        print("Start_Pressed - Controller")
-        # Altera a label para mostrar o programa em funcionamento
+    def start(self, caminho: str, impressora: str) -> None:
+        self.running = True
+        self.pt = PrintThread(caminho_processo=caminho, impressora=impressora)
+        default_printer = win32print.GetDefaultPrinter()
+        processo = self.printer.pegar_nome_processo(caminho)
+        self.pt.start()
         self.frame.m_staticText31.SetLabelText(
-            text="Programa em funcionamento")
-        self.running = True  # Variavel para rodar o loop do programa
-        print(impressora, caminho)
-        return True
+            text="Programa Inicializado")
 
     def stop(self):
-        self.running = False
+        self.pt.to_stop = True
         self.frame.m_staticText31.SetLabelText(text="Programa Parado")
         print("Stop_Pressed - Controller")
 
@@ -85,9 +115,12 @@ class PrinterController:
         # Retorna uma lista com caminho na posição [0], impressora [1]
         return [config_caminho.split("=")[1].replace('\n', ''),
                 config_impressora.split("=")[1]]
+            
 
+    def run(self):
+        self.frame.Show()
+        self.app.MainLoop()
 
 if __name__ == "__main__":
-    printer = PrinterController()
-    printer.frame.Show()
-    printer.app.MainLoop()
+    controller = PrinterController()
+    controller.run()
